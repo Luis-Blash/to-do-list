78cedd9edd1719d8c66505399fbbf93a739ee26e
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-03-23 18:37:55 -0600

    Se arreglo index.html y index.js Se volvio mas legible y cada uno de los botones tiene el addlistener Todo funciona en una sola pagina. Falta darle estilos

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,155 +1,120 @@
-window.addEventListener('load',()=>{
+window.addEventListener("load", () => {
     const URL_BACKEND = "http://localhost:5000/";
-    // Index
-    function index(){
-        return fetch(URL_BACKEND,{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: "include"
+    const MENSAJE_FETCH = document.getElementById('mensaje_fetch');
+    //Index
+    fetch(URL_BACKEND, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    })
+    .then((data) => data.json())
+    .then((respuesta) => {
+        let titulo_api = document.getElementById("titulo_api");
+        titulo_api.innerHTML = respuesta["mensaje"];
+    }).catch((error) => console.log(error));
+
+    // Insertar fetch
+    let formulario_post = document.getElementById("postFormulario");
+    formulario_post.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let datos_formulario = JSON.stringify({
+            titulo: document.getElementById("titulo_post").value,
+            descripcion: document.getElementById("descripcion_post").value,
         });
-    }
-    // GET ALL
-    function getTodos(){
-        return fetch((URL_BACKEND + "tarea"),{
-            method: 'GET',
+        fetch(URL_BACKEND + "tarea", {
+            method: "POST",
+            body: datos_formulario,
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            credentials: "include"
-        });
-    }
-    // PUT estado
-    function putEstado(id){
-        let url = URL_BACKEND + "tarea-estado/"+id
-        return fetch(url,{
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: "include"
-        });
-    }
-    // PUT Titulo,Descripcion
-    function putDatos(id,datos) {
-        let url = URL_BACKEND+"tarea/"+id
-        return fetch(url,{
-            method: 'PUT',
-            body: JSON.stringify(datos),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: "include"
+            credentials: "include",
         })
-    }
-    // ID
-    function peticionID(id,metodo) {
-        let url = URL_BACKEND+"tarea/"+id
-        return fetch(url,{
-            method: metodo,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: "include"
-        });
-    }
+        .then(data => data.json())
+        .then(respuesta => {
+            MENSAJE_FETCH.innerHTML = "Se Inserto a la base";
+            formulario_post.reset();
+        })
+        .catch((error) => console.log(error));
+    });
 
-    // Peticion fetch ----------------------
-    let app = document.getElementById("app");
-    // Index
-    index()
-    .then(data => data.json())
-    .then(mensaje =>{
-        let h1 = document.createElement("h1");
-        h1.innerText = mensaje["mensaje"];
-        app.append(h1);
-        return getTodos();
+    // GET todas las tareas
+    fetch(URL_BACKEND+'tarea', {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
     })
-    // Tareas
     .then(data => data.json())
-    .then(tareas => {
-        tareasAll(tareas);
-    })
+    // Aqui se crea la etiquetas de las tareas individuales
+    .then(respuesta => {
+        respuesta.forEach(element => {
+            //id 
+            let id = element['_id']['$oid'];
+            // Detectar id de la etiqueta todas las tareas 
+            let todas_las_tareas = document.getElementById('todas_las_tareas');
+            let tareas_individuales = document.createElement('div');
+            tareas_individuales.className = 'tareas_individuales';
+            tareas_individuales.id = id+"tarea";
+            // Agregar tareas individuales a todas_las_tareas
+            todas_las_tareas.append(tareas_individuales);
+            // Creando etiquetas div
+            let titulo_tareas = document.createElement('div');
+            titulo_tareas.className = 'titulo_tareas';
+            let descripcion_tareas = document.createElement('div');
+            descripcion_tareas.className = 'descripcion_tareas';
+            let botones = document.createElement('div');
+            botones.className = 'botones';
+            // Agregar las etiquetas creadas a todas las tareas_individuales
+            tareas_individuales.append(titulo_tareas);
+            tareas_individuales.append(descripcion_tareas);
+            tareas_individuales.append(botones);
 
-    // Creacion de elementos -------------------
-    // ver Todas las tareas fetch
-    function tareasAll(tareas){
-        // div tareas
-        let div_todas_tareas = document.getElementById("div_todas_tareas");
-        
-        // for de tareas
-        tareas.forEach(element => {
-            // div tareas individual
-            let indivual_tareas = document.createElement("div");
-            indivual_tareas.className =  "indivual_tareas";
-            div_todas_tareas.append(indivual_tareas)
-    
-            let titulo = document.createElement("h2")
-            let fecha = document.createElement("p")
-            let estado = document.createElement("button")
-            let ver = document.createElement("button")
+            // crear etiquetas que estaran en div titulo tareas
+            let h2 = document.createElement('h2');
+            let span1 = document.createElement('span');
+            // crear etiquetas que estaran en div descripcion tareas
+            let p = document.createElement('p');
+            // crear etiquetas que estaran en div botones
+            let button1 = document.createElement('button');
+            let button2 = document.createElement('button');
+            let button3 = document.createElement('button');
 
-            indivual_tareas.id = element['_id']['$oid'];
-            titulo.innerHTML =  element["titulo"];
+            h2.innerHTML = element['titulo'];
             let aux = new Date(element['fecha_creacion']['$date']);
-            fecha.innerHTML =  aux.getDate() + "/" + (aux.getMonth()+1)+ "/" + aux.getFullYear();
-            estado.innerHTML = tipoEstado(element["estado"])
-            ver.innerHTML = "Ver"
-    
-            indivual_tareas.append(titulo);
-            indivual_tareas.append(fecha);
-            // div button
-            let div_button = document.createElement("div");
-            div_button.className = "div_button";
-            indivual_tareas.append(div_button);
-    
-            div_button.append(estado);
-            div_button.append(ver);
+            span1.innerHTML = aux.getDate() + "/" + (aux.getMonth()+1)+ "/" + aux.getFullYear();
+            // Metodo para poner activado o finalizado
+            p.innerHTML = element['descripcion'];
 
-            id = element['_id']['$oid']
-            verDetalles(ver,id);
-            cambioEstado(estado,id);
+            let estado = tipoEstado(respuesta['estado']);
+            button1.innerHTML = "Editar";
+            button1.id = id+"1";
+            button1.addEventListener('click',()=>{crearFormularioParaEditar(id)});
+
+            button2.id = id+"2";
+            button2.innerHTML = estado;
+            button2.classList = estado;
+            button2.addEventListener('click',()=>{cambioEstado(id)});
+
+            button3.className = "eliminar-boton";
+            button3.innerHTML = "Eliminar";
+            button3.addEventListener("click",()=>{eliminarTarea(id)});
+
+            
+            // agrego a etiquetas a sus respectivos div
+            titulo_tareas.append(h2);
+            titulo_tareas.append(span1);
+            descripcion_tareas.append(p);
+            botones.append(button1);
+            botones.append(button2);
+            botones.append(button3);
         });
-    }
-    
-    // boton ver fetch
-    function verDetalles(boton,id){
-        boton.addEventListener('click', ()=>{
-            peticionID(id,'GET')
-            .then(data => data.json())
-            .then(tarea => {
-                let form = document.getElementById("putFormulario");
-                form.className = "no_ver";
-                let ver_tareas = document.getElementById("ver_tareas");
-                ver_tareas.classList.remove("no_ver");
-                let mensaje_fetch = document.getElementById("mensaje_fetch");
-                mensaje_fetch.className = "no_ver";
+    }).catch((error) => console.error(error));
 
-                let titulo = document.getElementById("h2_titulo");
-                let fecha = document.getElementById("span_fecha");
-                let descripcion = document.getElementById("p_descripcion");
-                let estado = document.getElementById("span_estado");
-                let eliminar = document.getElementById("boton_eliminar");
-                let editar = document.getElementById("boton_editar");
 
-                titulo.innerHTML =  tarea['titulo'];
-                let aux = new Date(tarea['fecha_creacion']['$date']);
-                fecha.innerHTML =  aux.getDate() + "/" + aux.getMonth() + "/" + aux.getFullYear();
-                descripcion.innerHTML = tarea['descripcion'];
-                estado.innerHTML = tipoEstado(tarea["estado"]);
-                eliminar.innerHTML = "Eliminar";
-                editar.innerHTML = "Editar";
-
-                id = tarea['_id']['$oid'];
-
-                eliminarTarea(eliminar,id);
-                editarTarea(editar,id);
-            })
-        })
-    }
-
-    // tipo de estado, activo o desactivado
+    // Detecta que tipo de estado y en vez de decir true o false, pone un texto
     function tipoEstado(estado){
         if(estado){
             return "Activo";
@@ -159,61 +124,95 @@ window.addEventListener('load',()=>{
     }
 
     // Cambio de estado fetch
-    function cambioEstado(boton,id){
-        boton.addEventListener('click', ()=>{
-            putEstado(id)
-            .then(response => response.json())
-            .then(respuesta => {
-                boton.innerHTML = tipoEstado(respuesta['estado'])
-            })
+    function cambioEstado(id){
+        fetch(URL_BACKEND+'tarea-estado/'+id,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include"
         })
+        .then(response => response.json())
+        .then(respuesta => {
+            let estado = tipoEstado(respuesta['estado']);
+            let boton = document.getElementById((id+"2"));
+            boton.innerHTML = estado;
+            boton.className = estado;
+        }).catch((error) => console.error(error));
     }
-    
-    // Eliminar fetch
-    function eliminarTarea(boton,id) {
-        boton.addEventListener('click', ()=>{
-            peticionID(id,'DELETE')
-            .then(response => response.json())
-            .then(respuesta => {
-                let mensaje_fetch = document.getElementById("mensaje_fetch");
-                let tarea_vista = document.getElementById(id);
-                let form = document.getElementById("putFormulario");
-                form.className = "no_ver";
-                tarea_vista.remove();
-                mensaje_fetch.classList.remove("no_ver");
-                mensaje_fetch.innerHTML = respuesta['mensaje'];
-                let ver_tareas = document.getElementById("ver_tareas");
-                ver_tareas.className = "no_ver";
-            })
+    // Crear Formulario Editar
+    function crearFormularioParaEditar(id){
+        fetch(URL_BACKEND+'tarea/'+id,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include"
         })
+        .then(data => data.json())
+        .then(respuesta => {
+            // Donde se crear el formulario
+            let tareas_individual = document.getElementById((id+"formulario"));
+            if(document.getElementById(id+'put') == null){
+                // Crear etiquetas
+                let formulario_put = document.createElement('form');
+                let titulo_put =  document.createElement('input');
+                let descripcion_put =  document.createElement('input');
+                let boton_put =  document.createElement('button');
+                // detalles
+                formulario_put.id = id+"put";
+                titulo_put.id = id+"titulo";
+                descripcion_put.id = id+"descripcion";
+                titulo_put.type = 'text';
+                titulo_put.value = respuesta['titulo'];
+                descripcion_put.type = 'text';
+                descripcion_put.value = respuesta['descripcion'];
+                boton_put.type = "submit";
+                boton_put.innerHTML = "Editar";
+                formulario_put.addEventListener('submit',(e)=>{editarTarea(id)});
+                // agregar a la etiqueta
+                formulario_put.append(titulo_put);
+                formulario_put.append(descripcion_put);
+                formulario_put.append(boton_put);
+                tareas_individual.append(formulario_put);
+            }else{
+                tareas_individual.removeChild(document.getElementById(id+'put'));
+            }
+        }).catch((error) => console.error(error));
+    }
+    // editar Fetch
+    function editarTarea(id){
+        let datos_formulario = JSON.stringify({
+            titulo: document.getElementById(id+"titulo").value,
+            descripcion: document.getElementById(id+"descripcion").value,
+        });
+        fetch(URL_BACKEND+"tarea/"+id,{
+            method: 'PUT',
+            body: datos_formulario,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include"
+        })
+        .then(data => data.json())
+        .then(respuesta => {
+            MENSAJE_FETCH.innerHTML = respuesta['mensaje'];
+        }).catch((error) => console.error(error));
     }
 
-    // Editar
-    const form = document.getElementById("putFormulario");
-    function editarTarea(boton,id) {
-        boton.addEventListener('click',()=>{
-            let form = document.getElementById("putFormulario");
-            peticionID(id,'GET')
-            .then(data => data.json())
-            .then(respuesta => {
-                form.classList.remove = "no_ver";
-                form.className = "formulario";
-                document.getElementById("titulo_input").value = respuesta['titulo'];
-                document.getElementById("descripcion_textarea").value = respuesta['descripcion'];
-                document.getElementById("sumbitId").value = respuesta['_id']['$oid'];
-            })
-        });
+    // Eliminar fetch
+    function eliminarTarea(id) {
+        fetch(URL_BACKEND+"tarea/"+id,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: "include"
+        }).then(data => data.json())
+        .then(respuesta => {
+            MENSAJE_FETCH.innerHTML = respuesta['mensaje'];
+            let eliminar = document.getElementById(id+"tarea");
+            eliminar.remove();
+        }).catch((error) => console.error(error));
     }
-    // PUT edit
-    form.addEventListener("submit",(e)=>{
-        //e.preventDefault()
-        if(document.getElementById("sumbitId").value != ""){
-            let id = document.getElementById("sumbitId").value;
-            let datos = {
-                "titulo": document.getElementById("titulo_input").value,
-                "descripcion": document.getElementById("descripcion_textarea").value
-            }
-            putDatos(id,datos);
-        }
-    })
 });
